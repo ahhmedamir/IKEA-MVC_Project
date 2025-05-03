6e8cfa4538a0b5dfb98b3defe9db184e9ee6ed8c
Ahmed Amir <150551966+ahhmedamir@users.noreply.github.com>
2025-05-03 21:06:00 +0300

    Delete IKEA.PL directory

--- IKEA.PL/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace IKEA.PL.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
